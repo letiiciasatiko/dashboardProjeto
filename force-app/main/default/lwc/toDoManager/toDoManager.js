7c9f48909447b9d304ed59a95abfe8feb9172934
Leticia <leticiasatiko@gmail.com>
2024-12-23 16:02:26 -0300

    segundo commit

--- force-app/main/default/lwc/toDoManager/toDoManager.js
+++ force-app/main/default/lwc/toDoManager/toDoManager.js
@@ -1,5 +1,38 @@
 import { LightningElement } from 'lwc';
 
 export default class ToDoManager extends LightningElement {
-    
+    time = "8:15";
+    greeting = "Good Evening";
+
+    getTime(){
+        const date = new Date();
+        const hour = date.getHours();
+        const min = date.getMinutes();
+
+        this.time = `${this.getHour(hour)}:${this.getDoubleDigit(min)}`
+
+        this.setGreeting(hour);
+    }
+
+    connectedCallback(){
+        this.getTime();
+    }
+
+    getHour(hour){
+        return hour === 0 ? 24:hour > 24 ? (hour-24) : hour;
+    }
+
+    getDoubleDigit(digit){
+        return digit<10 ? "0"+digit : digit; 
+    }
+
+    setGreeting(hour){
+        if (hour < 12){
+            this.greeting = "Bom dia!";
+        } else if (hour >= 12 && hour < 17){
+            this.greeting = "Boa Tarde!";
+        } else {
+            this.greeting = "Boa Noite!";
+        }
+    }
 }
