e46e61c076cea43c84ae322bc52c54b745c935f5
Leticia <leticiasatiko@gmail.com>
2025-01-07 16:02:59 -0300

    01_07-01

--- force-app/main/default/lwc/dashboardProdutoresTransacionadorBarra/dashboardProdutoresTransacionadorBarra.js
+++ force-app/main/default/lwc/dashboardProdutoresTransacionadorBarra/dashboardProdutoresTransacionadorBarra.js
@@ -1,3 +1,16 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import getProdutoresCount from '@salesforce/apex/dashboardController.getProdutoresCount';
 
-export default class DashboardProdutoresTransacionadorBarra extends LightningElement {}
+export default class DashboardProdutoresCount extends LightningElement {
+    produtoresCount;
+
+    @wire(getProdutoresCount)
+    wiredCount({ error, data }) {
+        if (data) {
+            this.produtoresCount = data;  // Agora, data já é o valor numérico do contador
+        } else if (error) {
+            this.produtoresCount = undefined;
+            console.error(error);
+        }
+    }
+}
