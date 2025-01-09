e1f28b04feba424043f90911f9e99ebdd7fa530f
Leticia <leticiasatiko@gmail.com>
2025-01-09 14:32:40 -0300

    03_09-01

--- force-app/main/default/lwc/dashboardProdutoresNaoVisitaveisIndicadorNumerico/dashboardProdutoresNaoVisitaveisIndicadorNumerico.js
+++ force-app/main/default/lwc/dashboardProdutoresNaoVisitaveisIndicadorNumerico/dashboardProdutoresNaoVisitaveisIndicadorNumerico.js
@@ -1,17 +1,20 @@
 import getNaoVisitaveis from '@salesforce/apex/dashboardController.getNaoVisitaveis';
-import { LightningElement, wire } from 'lwc'; 
+import { LightningElement, wire } from 'lwc';
 import { loadScript } from 'lightning/platformResourceLoader';
-import ChartJs from '@salesforce/resourceUrl/ChartJs';
-
+import ChartJs from '@salesforce/resourceUrl/chartjs';
+ 
 export default class DashboardProdutoresNaoVisitaveisIndicadorNumerico extends LightningElement {
     naoVisitCount = 0;
+    chartJsLib = 0;
 
-    @wire(getNaoVisitaveis)
-    wiredNaoVisitaveisCount({ error, data }){
-        if(data){
-            this.naoVisitCount = data;
-        }else if(error){
-            console.error(error);
-        }
-    }
+
+
+    // @wire(getNaoVisitaveis)
+    // wiredNaoVisitaveisCount({ error, data }){
+    //     if(data){
+    //         this.naoVisitCount = data;
+    //     }else if(error){
+    //         console.error(error);
+    //     }
+    // }
 }
